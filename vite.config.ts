d0ed92429a16c88cef4258d59922d9a2d19c8d3c
Bira <umorais@gmail.com>
2026-07-30 15:10:08 -0300

    fix: corrige entry point no index.html de App.tsx para main.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-  import react from '@vitejs/plugin-react'
-
 
 function figmaAssetResolver() {
   return {
@@ -20,18 +19,13 @@ export default defineConfig({
   base: '/sementesdehumildade/',
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
     tailwindcss(),
+    react(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
